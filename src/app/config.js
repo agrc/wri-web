6b118a583e65307fa0dea82aee4767d0c19f36bd
steveoh <sgourley@utah.gov>
2015-07-06 17:24:58 -0600

    Release v0.3.0

--- src/app/config.js
+++ src/app/config.js
@@ -57,7 +57,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '0.2.0',
+        version: '0.3.0',
 
         // apiKey: String
         //      The api key used for services on api.mapserv.utah.gov
