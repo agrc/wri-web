b3c4438d6557544289282d6bd25c884e720a280a
steveoh <sgourley@utah.gov>
2015-08-13 10:36:16 -0600

    update project query layer query
    add L option to repoint mxd
    [ci skip]

--- maps/RepointMXD.py
+++ maps/RepointMXD.py
@@ -10,12 +10,16 @@ local = db + 'WRI_LOCAL.sde'
 dev = db + 'WRI_DEV as wri_user.sde'
 at = db + 'WRI_AT as wri_user.sde'
 
-target = raw_input('Dev (D) or AT (A)? ')
+target = raw_input('Local (L), Dev (D), or AT (A)? ')
 
 if target == 'D':
     dest = dev
-else:
+elif target == 'A':
     dest = at
+else:
+    dest = local
+
+print('updating mxd\'s to use {}'.format(dest))
 
 for f in glob.glob('*.local.mxd'):
     print(f)
