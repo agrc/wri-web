d13c33b082cc7591b3c63e3864fc00ff9c19b968
steveoh <sgourley@utah.gov>
2015-10-07 08:49:27 -0600

    Release v0.6.2

--- src/app/config.js
+++ src/app/config.js
@@ -60,7 +60,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '0.6.1',
+        version: '0.6.2',
 
         // scaleTrigger: Number
         //      the basemap level to toggle centroids
