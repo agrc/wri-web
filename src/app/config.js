9f3a33536386c2ea1a0d50805e31de2d904b75d7
steveoh <sgourley@utah.gov>
2015-12-14 11:56:25 -0700

    Release v0.15.0

--- src/app/config.js
+++ src/app/config.js
@@ -76,7 +76,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '0.14.3',
+        version: '0.15.0',
 
         // quadWord: String
         //      The quad word for using appliance layers
